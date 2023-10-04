dff9a592a6e2622f8998ae30eedea3e66bdcfa4a
rdiazbarron <diazbarronricardojairo@gmail.com>
2023-10-04 19:42:43 -0400

    estilos arqui android

--- Tarea_Estilos_Arqui/Ejercicio2/MainActivity.java
+++ Tarea_Estilos_Arqui/Ejercicio2/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.combinatoria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonArqui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonArqui = findViewById(R.id.buttonArqui);
+        buttonArqui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ArquiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
